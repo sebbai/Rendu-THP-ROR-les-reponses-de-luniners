3e02fc1ec5c83312202c1d3d6251ac2cd1d10c01
bseb <bailly93@gmail.com>
2018-02-05 19:55:44 +0100

    password en console

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find.params[:id]
+	end
+
+	def new 
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params_user)
+		if @user.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+		def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(params_user)
+			redirect_to root_path
+		else
+			redirect_to 'user#edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def params_user
+		params.require(:user).permit(:email)
+	end
+end
